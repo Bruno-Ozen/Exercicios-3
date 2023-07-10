26162c304b4995497476984bbdc9226e4ead3513
Bruno Henrique de Pinho <112650688+Bruno-Ozen@users.noreply.github.com>
2023-07-10 20:24:56 -0300

    Ex 005 e 006

---
+++ ex005.py
@@ -0,0 +1,4 @@
+valor = float(input("Reais -> Dolar. Digite o valor (R$): "))
+dolar:float = 5.0
+conversao:float = valor/dolar
+print("O valor digitado em dólar é $", conversao)
---
+++ ex006.py
@@ -0,0 +1,4 @@
+print("X^2+bX+c = 0")
+a = float(input("Digite o valor de A: "))
+b = float(input("Digite o valor de B: "))
+c = float(input("Digite o valor de C: "))
