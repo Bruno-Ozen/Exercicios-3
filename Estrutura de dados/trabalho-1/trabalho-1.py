63df7ef8fe542156f9938d0e5a6154bd5e5fa319
Bruno Henrique de Pinho <112650688+Bruno-Ozen@users.noreply.github.com>
2024-01-11 21:04:23 -0300

    Implementação da classe Mercado

--- Estrutura de dados/trabalho-1/trabalho-1.py
+++ Estrutura de dados/trabalho-1/trabalho-1.py
@@ -8,33 +8,74 @@
 
 from fila import FilaEstatica
 from random import randint
+from vetor import Vetor
+from typing import List
 
 class Cliente:
     def __init__(self, preferencial: bool, senha: int) -> None:
         self.preferencial = preferencial
         self.senha = senha
 
-class Caixa:
 
-    senhas: int = []
+class Caixa:
+    senhas_clientes: FilaEstatica
+    senhas_disponiveis: FilaEstatica
+
+    def __init__(self) -> None:
+        self.senhas_clientes = FilaEstatica(10)
+        self.senhas_disponiveis = FilaEstatica(10)
+        for i in range(1, 10):
+            self.senhas_disponiveis.enfileira(i)
+
+    def geraSenha(self, cliente: Cliente) -> int:
+        self.senhas_clientes.enfileira(self.senhas_disponiveis.primeiroElemento)
+
+    def disponivel() -> bool:
+        return not senhas_clientes.cheia()
+class Mercado:
+    caixas: List[Caixa]
 
     def __init__(self, quantidade: int) -> None:
         if quantidade >= 2 and quantidade <= 20:
             self.quantidade = quantidade
+            caixas = Vetor(self.quantidade)
+            for i in range(1, self.quantidade):
+                caixas[i] = Caixa()
         else:
             raise ValueError('Só serão permitidos de 2 a 20 caixas para o atendimento.')
+        
+    def logistica() -> int:
+        ''' Função responsável pela logística do mercado. Verifica qual é o primeiro caixa com vaga disponível e
+            retorna a senha para o cliente:
+        '''
+        disponivel: bool
+        i: int = 0
+        while not disponivel:
+            if self.caixas
+            i += 1
 
-    def geraSenha(self, cliente: Cliente) -> int:
-        if len(self.senhas) <= self.quantidade
-            cliente.senha = randint(0, self.quantidade)
-            self.senhas.append(cliente.senha)
-        else:
+
 
 def menu():
     print('------- SIMULADOR DE ATENDIMENTO DE CLIENTES --------')
     qtdCaixas = input('Digite a quantidade de caixas que estarão ativos no mercado:')
-    fila_clientes = FilaEstatica(qtdCaixas)
-    
+
+    mercado = Mercado(qtdCaixas)
+    resposta: int
+    while resposta != 5:
+        print('Digite:')
+        print('1) Para retirada de senha; ')
+        print('2) Para chamar um Cliente; ')
+        print('3) Para consultar os Clientes em espera;')
+        print('4) Para consultar o estado dos caixas;')
+        print('5) Para sair. ')
+        resposta = input()
+        if type(resposta) is not int:
+            print('Valor inválido. Digite um número das opções acima.')
+        elif resposta == 1:
+            mercado.logistica()
+
+
 
 def main(args):
     menu()
